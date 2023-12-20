207cb78ddb4bbd1b70138691c6710d5c9b97e339
TheSecurityLead <sgrie10@wgu.edu>
2023-12-20 23:17:42 +0300

    Updated Server Web Address

--- src/App.jsx
+++ src/App.jsx
@@ -43,7 +43,7 @@ const CityExplorer = () => {
       setMapImage(mapUrl);
 
       // Fetch weather data from your Express server
-      const weatherResponse = await axios.get(`http://localhost:3001/weather`, {
+      const weatherResponse = await axios.get(`https://lab-07.onrender.com:3001/weather` , {
         params: {
           searchQuery: cityName,
           lat: firstLocation.lat,
